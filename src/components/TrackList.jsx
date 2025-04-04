065390d5ab4cfa77fb2541876725d86e1fa34576
Ramone Graham <ramonegraham@gmail.com>
2025-04-04 05:16:23 +0100

    Create TrackList styling

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,11 +1,13 @@
 //#region Internal project imports
 import Track from "./Track";
+
+import styles from "./TrackList.module.css";
 //#endregion
 
 //#region File body
 export default function TrackList({ tracks }) {
 	return (<ol>
-		{tracks.map(track => (<li key={track.id}>
+		{tracks.map(track => (<li key={track.id} className={styles.trackItem}>
 			<Track data={track} />
 		</li>))}
 	</ol>);
